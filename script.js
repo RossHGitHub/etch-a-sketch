348243e3542744b17a099e92e84e168fde5f0c8c
Ross Henderson <rossliamhenderson@gmail.com>
2022-07-11 14:31:38 +0100

    created basic html/css/js files - then created the first row of empty divs inside container

---
+++ script.js
@@ -0,0 +1,56 @@
+let body = document.querySelector('body');
+let container = document.getElementById('container');
+
+
+
+let rowOne = document.createElement('div');
+rowOne.classList = 'rowOne';
+container.appendChild(rowOne);
+
+let oneOne = document.createElement('div');
+rowOne.appendChild(oneOne);
+
+let oneTwo = document.createElement('div');
+rowOne.appendChild(oneTwo);
+
+let oneThree = document.createElement('div');
+rowOne.appendChild(oneThree);
+
+let oneFour = document.createElement('div');
+rowOne.appendChild(oneFour);
+
+let oneFive = document.createElement('div');
+rowOne.appendChild(oneFive);
+
+let oneSix = document.createElement('div');
+rowOne.appendChild(oneSix);
+
+let oneSeven = document.createElement('div');
+rowOne.appendChild(oneSeven);
+
+let oneEight = document.createElement('div');
+rowOne.appendChild(oneEight);
+
+let oneNine = document.createElement('div');
+rowOne.appendChild(oneNine);
+
+let oneTen = document.createElement('div');
+rowOne.appendChild(oneTen);
+
+let oneEleven = document.createElement('div');
+rowOne.appendChild(oneEleven);
+
+let oneTwelve = document.createElement('div');
+rowOne.appendChild(oneTwelve);
+
+let oneThirteen = document.createElement('div');
+rowOne.appendChild(oneThirteen);
+
+let oneFourteen = document.createElement('div');
+rowOne.appendChild(oneFourteen);
+
+let oneFifteen = document.createElement('div');
+rowOne.appendChild(oneFifteen);
+
+let oneSixteen = document.createElement('div');
+rowOne.appendChild(oneSixteen);
