e1b739bb569f64405b0ff67912f75f4df154e42f
Ross Henderson <rossliamhenderson@gmail.com>
2022-07-11 17:03:45 +0100

    Got the basic mechanic down. IE Evenly spaced divs, and added the class list changes when mouse over.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let body = document.querySelector('body');
 let container = document.getElementById('container');
 
-for (let i=0;i<256; i++){
+for (let i=0;i<256; i++) {
     let boxes = document.createElement('div');
     boxes.classList='boxes';
     boxes.textContent='.';
     container.appendChild(boxes);
-    
+    boxes.addEventListener('mouseover', () =>{boxes.classList = 'boxHover'});
 }
+
