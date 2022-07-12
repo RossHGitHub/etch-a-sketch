6ac4b3ee476b4dbacbd7dfaae5ee932abd721fc6
Ross Henderson <rossliamhenderson@gmail.com>
2022-07-12 23:02:37 +0100

    Added the adjustable aspect to create the responsive sizing of the divs via the inline-styling on the script.

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 let body = document.querySelector('body');
 let container = document.getElementById('container');
 
-for (let i=0;i<256; i++) {
+
+function generate(userAns) {
+    
+container.style.gridTemplateRows = `repeat(${userAns}, 1fr)`;
+container.style.gridTemplateColumns = `repeat(${userAns}, 1fr)`;
+
+for (let i=0;i<(userAns*userAns); i++) {
     let boxes = document.createElement('div');
     boxes.classList='boxes';
-    boxes.textContent='.';
     container.appendChild(boxes);
     boxes.addEventListener('mouseover', () =>{boxes.classList = 'boxHover'});
 }
+}
 
+let button = document.getElementById('btn');
+
+button.addEventListener('click', ()=>{
+    let userAns = prompt('New grid size? (Max 100)');
+
+    if(userAns>100){
+        alert('Too high! MAX: 100.')
+    } else{
+generate(userAns);}})
